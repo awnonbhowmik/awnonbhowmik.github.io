e81febf153f9a17011157bf445acc16e40a81142
awnonbhowmik <awnonbhowmik256@gmail.com>
2025-04-04 21:17:00 -0400

    Netlify automation

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"pJV+vZn0OePj6ZpMQ/YsWd3TgLuB+08hzRyrbrL043o=\"\n}"
